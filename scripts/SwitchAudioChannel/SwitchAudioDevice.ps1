5365e700a91822b285168b09138ccbd5c68eebdc
Eckhard Anders <eckhard@eckon.de>
2023-03-15 16:39:58 +0100

    feat: PowerShell script for audio output switch

---
+++ scripts/SwitchAudioChannel/SwitchAudioDevice.ps1
@@ -0,0 +1,29 @@
+﻿# 
+# Audio Switch
+# Switch through all audio channels which are used for playback by executing this script.
+# 
+# To switch your music between external speaker and your headset needs 3 clicks in Windows 11. 
+# This script automatically changes the output audio to the next channel, just execute until 
+# your favorite channel is used. 
+#
+# You can simply bind this script to a button or define a ke shortcut to execute the script.
+
+
+# Get audio output device used for playback
+$audioDevices = Get-AudioDevice -list | Where-Object -Property Type -eq "Playback"
+
+# Device channel currently used
+$currentAudioDevice = Get-AudioDevice -Playback
+
+# Identify the current channel in the devices list
+$foundObj = $audioDevices | Where-Object { $_.Index -eq $currentAudioDevice.Index }
+$foundIndex = $audioDevices.IndexOf($foundObj)
+
+# Find index of next channel or start again at index 0
+$nextIndex = ($foundIndex + 1) % $audioDevices.Count
+$nextAudioDevice = $audioDevices[$nextIndex]
+
+Write-Host "Switch Audio output to $($nextAudioDevice.Name)."
+
+# Set the next audio device
+Set-AudioDevice $nextAudioDevice.Index
---
+++ scripts/SwitchAudioChannel/readme.md
@@ -0,0 +1,23 @@
+# Audio Switch
+Switch through all audio channels which are used for playback by executing this script.
+
+To switch your music between external speaker and your headset needs 3 clicks in Windows 11. 
+This script automatically changes the output audio to the next channel, just execute until 
+your favorite channel is used. 
+
+## Installation
+
+Download SwitchAudioDevice.ps1 file to your Windows 10/11 PC.
+
+## Execution
+
+Rightclick  on ps1 file and Execute with Powershell. 
+
+Use the follow
+
+## Execute via shortcut
+
+Create a shortcut to the ps1 file and edit the shotcut. Changing the execution line to 
+> powershell -file "SwitchAudioDevice.ps1"
+
+And add your favorite keyboard shortcut. 
